aa6d696fa67304e8e6c536363b239de2946efafe
MENOUER1 <menouerr2@gmail.com>
2019-09-28 21:00:39 +0200

    test

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const button = document.querySelector("button");
 const li = document.querySelectorAll("li");
 
 let position = 0;
-
 // creation des constantes et variables
 
 
 function nextImage() {
     position++;
 // creation des fonctions
+
+
 
 if (position == li.length){
     position = 0;
@@ -23,4 +24,9 @@ for(let i = 0; i < li.length; i++) {
 li[position].classList.add('active');
 }
 // creation de la boucle for
+
+
+button.addEventListener("click", nextImage);
+// fonction a appeler
+
 
